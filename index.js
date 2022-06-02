8d0d1d67e53d64df05ba2a4fcc322692b073b3f1
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-06-02 16:13:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const clickInstruction = document.getElementById("click-instruction");
 // [ ] remove Google Tracking Script from end of index.html
 // [ ] update/translate specified tags in <head>  at top of index.html
 // [ ] change language and translator below as needed
-const language = "en";
-const translator = "sujato";
+const language = "de";
+const translator = "sabbamitta";
 // [ ] below needs to be translated when changing to a new language
 const disclaimer =
   "Disclaimer: This random sutta generator is not to be used to somehow get an answer from the universe as to what Dhamma we need to hear most at this moment in time. It’s just code. Better to ask a good kalyanamitta what Dhamma you need to reflect on.";
